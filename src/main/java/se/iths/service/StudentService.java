6037d02a3b74b2f357ef5cc7616fa0085a07608d
albindubech <albin.dubech@hotmail.com>
2022-03-03 20:06:49 +0100

    Add CRUD functionality in StudentService

--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,38 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.ws.rs.Path;
+import java.util.List;
+
+@Transactional
 public class StudentService {
 
+    @PersistenceContext
+    EntityManager entityManager;
 
+    public void createStudent(Student student) {
+        entityManager.persist(student);
+    }
+
+    public Student getStudentById(Long id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    public List<Student> getAllStudents() {
+//        return entityManager.createQuery("SELECT s FROM Student s", Student.class).getResultList();
+        return null;
+    }
+
+    public void updateStudent(Student student) {
+        entityManager.merge(student);
+    }
+
+    public void deleteStudent(Long id) {
+        Student foundStudent = entityManager.find(Student.class, id);
+        entityManager.remove(foundStudent);
+    }
 }
