f7e329d9c5c6e43039903a07e93cb917432f94d3
albindubech <albin.dubech@hotmail.com>
2022-03-03 20:08:30 +0100

    Add POST and GET in StudentRest

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,41 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("students")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    @Inject
+    StudentService studentService;
 
+    @Path("")
+    @POST
+    public Response createStudent(Student student) {
+        studentService.createStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Response getStudentById(@PathParam("id") Long id) {
+        Student foundStudent = studentService.getStudentById(id);
+        return Response.ok(foundStudent).build();
+    }
+
+    @Path("")
+    @GET
+    public Response getAllStudents() {
+        List<Student> foundStudents = studentService.getAllStudents();
+
+        return Response.ok(foundStudents).build();
+    }
 }
