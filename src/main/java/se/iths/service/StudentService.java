c44bb1e6846777c4b6c6b4c5305e1b0bf4a4b1d5
albindubech <albin.dubech@hotmail.com>
2022-03-06 20:47:42 +0100

    Add getStudentByLastName in StudentService

--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -5,8 +5,8 @@ import se.iths.entity.Student;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-import javax.ws.rs.Path;
 import java.util.List;
+import java.util.Optional;
 
 @Transactional
 public class StudentService {
@@ -18,13 +18,12 @@ public class StudentService {
         entityManager.persist(student);
     }
 
-    public Student getStudentById(Long id) {
-        return entityManager.find(Student.class, id);
+    public Optional<Student> getStudentById(Long id) {
+        return Optional.ofNullable(entityManager.find(Student.class, id));
     }
 
     public List<Student> getAllStudents() {
-//        return entityManager.createQuery("SELECT s FROM Student s", Student.class).getResultList();
-        return null;
+        return entityManager.createQuery("SELECT s FROM Student s", Student.class).getResultList();
     }
 
     public void updateStudent(Student student) {
@@ -34,5 +33,11 @@ public class StudentService {
     public void deleteStudent(Long id) {
         Student foundStudent = entityManager.find(Student.class, id);
         entityManager.remove(foundStudent);
+    }
+
+    public List<Student> getStudentByLastName(String lastName) {
+        return entityManager.createQuery("SELECT s FROM Student s WHERE s.lastName = :lastName", Student.class)
+                .setParameter("lastName", lastName)
+                .getResultList();
     }
 }
