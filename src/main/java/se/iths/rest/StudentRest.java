21d2fabbfa7c054e0dc2c63f681b333eee296a63
albin.dubech <albin.dubech@cgi.com>
2022-03-08 14:38:35 +0100

    Implement error handling and correct responsecodes

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,13 +1,16 @@
 package se.iths.rest;
 
 import se.iths.entity.Student;
+import se.iths.error.ErrorMessage;
+import se.iths.exceptions.StudentDataInvalidException;
+import se.iths.exceptions.StudentNotFoundException;
 import se.iths.service.StudentService;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
 import java.util.List;
+import java.util.Optional;
 
 @Path("students")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -20,15 +23,23 @@ public class StudentRest {
     @Path("")
     @POST
     public Response createStudent(Student student) {
+        if (student.getFirstName().isEmpty() || student.getLastName().isEmpty() || student.getEmail().isEmpty()) {
+            throw new StudentDataInvalidException(new ErrorMessage("401", "Required data is missing", "/api/v1/students"));
+        }
+
         studentService.createStudent(student);
-        return Response.ok(student).build();
+        return Response.ok().status(Response.Status.CREATED).build();
     }
 
     @Path("{id}")
     @GET
-    public Response getStudentById(@PathParam("id") Long id) {
-        Student foundStudent = studentService.getStudentById(id);
-        return Response.ok(foundStudent).build();
+    public Response getStudent(@PathParam("id") Long id) {
+        Optional<Student> foundStudent = studentService.getStudentById(id);
+
+        if (foundStudent.isEmpty())
+            throw new StudentNotFoundException(new ErrorMessage("404", "Student with id: " + id + " was not found", "/api/v1/students/" + id));
+
+        return Response.ok(foundStudent).status(Response.Status.FOUND).build();
     }
 
     @Path("")
@@ -36,6 +47,47 @@ public class StudentRest {
     public Response getAllStudents() {
         List<Student> foundStudents = studentService.getAllStudents();
 
+        if (foundStudents.isEmpty())
+            throw new StudentNotFoundException(new ErrorMessage("404", "No students found", "123"));
+
         return Response.ok(foundStudents).build();
+    }
+
+    @Path("lastname")
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    public Response getStudentByLastName(@QueryParam("lastname") String lastname) {
+        List<Student> foundStudents = studentService.getStudentByLastName(lastname);
+
+        if (foundStudents.isEmpty())
+            throw new StudentNotFoundException(new ErrorMessage("404"
+                    , "No students with lastname: " + lastname + " was found!"
+                    , "/api/v1/lastname?lastName=" + lastname));
+
+        return Response.ok(foundStudents).build();
+    }
+
+    @Path("{id}")
+    @DELETE
+    public Response deleteStudent(@PathParam("id") Long id) {
+        Optional<Student> foundStudent = studentService.getStudentById(id);
+
+        if (foundStudent.isEmpty())
+            return Response.ok().status(Response.Status.NO_CONTENT).build();
+
+        studentService.deleteStudent(id);
+        return Response.ok().build();
+    }
+
+    @Path("{id}")
+    @PUT
+    public Response updateStudent(@PathParam("id") Long id, Student student) {
+        Optional<Student> foundStudent = studentService.getStudentById(id);
+
+        if (foundStudent.isEmpty())
+            throw new StudentNotFoundException(new ErrorMessage("404", "Student with id: " + id + " was not found", "/api/v1/students/" + id));
+
+        studentService.updateStudent(student);
+        return Response.ok(student).build();
     }
 }
