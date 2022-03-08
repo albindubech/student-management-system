80d2311b8be5c0a621ae6758f0fb7cf0e3aea4f4
albin.dubech <albin.dubech@cgi.com>
2022-03-08 14:24:36 +0100

    Add @NotEmpty notation to entity fields

--- src/main/java/se/iths/entity/Student.java
+++ src/main/java/se/iths/entity/Student.java
@@ -1,17 +1,24 @@
 package se.iths.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
+@Entity
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    @NotEmpty
     private String firstName;
+    @NotEmpty
     private String lastName;
+    @NotEmpty
+    @Email
     private String email;
     private String phoneNumber;
 
